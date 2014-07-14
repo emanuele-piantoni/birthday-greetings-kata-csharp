07be4b02567cda34fd04c0767a6495cf318163fc
Matteo Vaccari <vaccari@pobox.com>
2014-07-14 10:50:26 +0200

    Initial Import

---
+++ BirthdayGreetings/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BirthdayGreetings
+{
+	public class Employee
+	{
+		private XDate birthDate;
+		private String lastName;
+		private String firstName;
+		private String email;
+
+		public Employee (String firstName, String lastName, String birthDate, String email) 
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.birthDate = new XDate(birthDate);
+			this.email = email;
+		}
+
+		public bool isBirthday(XDate today) {
+			return today.isSameDay(birthDate);
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+	}
+}
+
